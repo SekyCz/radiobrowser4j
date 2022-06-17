a0c6f1119bd033f3bba5d82bbe529418f7d5a362
Tomáš Sekera <tomas.sekera@avast.com>
2022-06-17 10:21:04 +0200

    Fix collection map with duplicate keys
    Merge rule has been added.

--- radiobrowser4j/src/main/java/de/sfuhrm/radiobrowser4j/RadioBrowser.java
+++ radiobrowser4j/src/main/java/de/sfuhrm/radiobrowser4j/RadioBrowser.java
@@ -188,7 +188,8 @@ public final class RadioBrowser {
             return map.stream()
                     .collect(Collectors.toMap(
                             m -> m.get("name"),
-                            m -> Integer.parseInt(m.get("stationcount"))));
+                            m -> Integer.parseInt(m.get("stationcount")),
+                            (m1, m2) -> m1));
         } finally {
             close(response);
         }
@@ -229,7 +230,8 @@ public final class RadioBrowser {
             return map.stream()
                     .collect(Collectors.toMap(
                             m -> m.get("name"),
-                            m -> Integer.parseInt(m.get("stationcount"))));
+                            m -> Integer.parseInt(m.get("stationcount")),
+                            (m1, m2) -> m1));
         } finally {
             close(response);
         }
